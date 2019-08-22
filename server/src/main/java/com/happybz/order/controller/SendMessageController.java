b0a4e4035c87c00fe8b89bcad4a327f5ce9e7d28
Happy_Baozi <happy_bz@qq.com>
2019-08-22 21:27:24 +0800

    RabbitMQ的基本使用 + Spring Cloud Stream的使用

---
+++ server/src/main/java/com/happybz/order/controller/SendMessageController.java
@@ -0,0 +1,34 @@
+package com.happybz.order.controller;
+
+import com.happybz.order.dto.OrderDTO;
+import com.happybz.order.message.StreamClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class SendMessageController {
+
+    @Autowired
+    private StreamClient streamClient;
+
+//    @GetMapping("/sendMessage")
+//    public void process() {
+//        String message = "now " + new Date();
+//        streamClient.output().send(MessageBuilder.withPayload(message).build());
+//    }
+
+    /**
+     * 发送 orderDTO对象
+     */
+    @GetMapping("/sendMessage")
+    public void process() {
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setOrderId("123456");
+        streamClient.output().send(MessageBuilder.withPayload(orderDTO).build());
+    }
+
+}
